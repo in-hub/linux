2174f6018a9b67dec22adb04362990183c533ad6
Markus Elfring <elfring@users.sourceforge.net>
2019-11-06 19:12:30 +0100

    bus: ti-sysc: Adjust exception handling in sysc_child_add_named_clock()
    Add a jump target so that a call of the function “clk_put”
    can be better reused at the end of this function.
    
    Signed-off-by: Markus Elfring <elfring@users.sourceforge.net>
    Signed-off-by: Tony Lindgren <tony@atomide.com>
    (cherry picked from commit cb6cfe2eaed171b5a2e575fa3f0cf0924b5bd1d2)

--- drivers/bus/ti-sysc.c
+++ drivers/bus/ti-sysc.c
@@ -1951,9 +1951,8 @@ static int sysc_child_add_named_clock(struct sysc *ddata,
 
 	clk = clk_get(child, name);
 	if (!IS_ERR(clk)) {
-		clk_put(clk);
-
-		return -EEXIST;
+		error = -EEXIST;
+		goto put_clk;
 	}
 
 	clk = clk_get(ddata->dev, name);
@@ -1963,7 +1962,7 @@ static int sysc_child_add_named_clock(struct sysc *ddata,
 	l = clkdev_create(clk, name, dev_name(child));
 	if (!l)
 		error = -ENOMEM;
-
+put_clk:
 	clk_put(clk);
 
 	return error;
